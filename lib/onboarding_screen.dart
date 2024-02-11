3d6cd789542edd817c107e0efb4902da5238cbce
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2024-02-11 13:25:49 +0530

    Completed the onboarding screen

---
+++ lib/onboarding_screen.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+
+class OnboardingScreen extends StatelessWidget {
+  const OnboardingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: 273,
+              width: 300,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage(
+                    "assets/onboarding.png",
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 38,
+            ),
+            const Text(
+              "Stay on top of your finance with us.",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                height: 1.2,
+                color: Colors.black,
+                fontSize: 34,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            const Padding(
+              padding: EdgeInsets.symmetric(horizontal: 60.0),
+              child: Text(
+                "We are your new financial Advisors to recommend the best investments for you.",
+                style: TextStyle(
+                  color: Color(
+                    0xff4F4F4F,
+                  ),
+                  fontSize: 17,
+                  fontWeight: FontWeight.w500,
+                  height: 1.1,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            InkWell(
+              onTap: () {
+                // TODO: Add navigation
+              },
+              child: Container(
+                alignment: Alignment.center,
+                margin: const EdgeInsets.symmetric(
+                  horizontal: 26,
+                ),
+                height: 60,
+                width: MediaQuery.of(context).size.width,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(
+                    16,
+                  ),
+                  color: const Color(
+                    0xff31A062,
+                  ),
+                ),
+                child: const Text(
+                  "Create account",
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 18,
+            ),
+            InkWell(
+              onTap: () {
+                // TODO: Add navigation
+              },
+              child: const Text(
+                "Login",
+                style: TextStyle(
+                  color: Color(0xff31A062),
+                  fontSize: 17,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
