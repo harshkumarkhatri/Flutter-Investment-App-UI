42a58279e607e0e40646164d7f07c4b290a45ecc
Harsh Kumar Khatri <mailharshkhatri@gmail.com>
2024-02-12 23:50:14 +0530

    Started with contact info screen

---
+++ lib/contact_info_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ContactInfoScreen extends StatelessWidget {
+  const ContactInfoScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
